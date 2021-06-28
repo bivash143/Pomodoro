c64fc96ee09b0a78c0bed58f4be27365678e1f6a
bivashchakraborty554 <bivashchakraborty554@gmail.com>
2021-06-28 06:36:01 +0530

    reset fun added

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 #! /usr/bin/python3
 from tkinter import *
+import math
 
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
-SHORT_BREAK_MIN = 2
-LONG_BREAK_MIN = 2
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
 CANVAS_CENTER_X = 394/2
 CANVAS_CENTER_Y = 428/2
 NO_OF_REPS = 0
+timer = None
 
 
 def start_fun():
@@ -28,8 +30,13 @@ def start_fun():
         title_label.config(text="Work", fg=GREEN)
         count_down(WORK_MIN * 60)
 
+
 def reset_fun():
-    pass
+    window.after_cancel(timer)
+    title_label.config(text="Timer")
+    canvas.itemconfig(timer_text, text="00:00")
+    check_label.config(text="")
+
 
 def count_down(count):
     count_min = count//60 # or math.floor( count/60)
@@ -40,9 +47,17 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count -1)
+        global timer
+        timer = window.after(1000, count_down, count -1)
     else:
         start_fun()
+        marks = ""
+        no_of_session = math.floor(NO_OF_REPS/2)
+
+        for _ in range(no_of_session):
+            marks += "✔"
+
+        check_label.config(text=marks)
 
 
 window = Tk()
@@ -55,6 +70,7 @@ title_label.grid(row=0, column=1)
 canvas = Canvas(width=394, height=428, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato2.png")
 canvas.create_image(CANVAS_CENTER_X, CANVAS_CENTER_Y, image=tomato_img)
+
 timer_text = canvas.create_text(CANVAS_CENTER_X, CANVAS_CENTER_Y+50, text="00:00", fill="white", font=(FONT_NAME, 45, "bold"))
 canvas.grid(row=1, column=1)
 
@@ -64,7 +80,7 @@ start_button.grid(row=2, column=0)
 check_label = Label(bg=YELLOW, fg=GREEN, font = (FONT_NAME, 20, "bold"))
 check_label.grid(row=3, column=1)
 
-rest_button = Button(text="Reset")
+rest_button = Button(text="Reset", command=reset_fun)
 rest_button.grid(row=2, column=2)
 
 
